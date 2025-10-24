e084445beea5c06fbf78c2c866221e87810ece1e
Michał Górny <mgorny@quansight.com>
2025-10-24 19:04:14 +0200

    Mention gxx & gfortran
    Suggested by @isuruf.
    
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- recipe/activate.ps1
+++ recipe/activate.ps1
@@ -1,4 +1,4 @@
 Write-Host "========================================================================"
 Write-Host "The 'm2w64-gcc' package is obsolete and is no longer updated."
-Write-Host "Please use 'gcc' instead."
+Write-Host "Please use 'gcc', 'gxx', 'gfortran' packages instead."
 Write-Host "========================================================================"
